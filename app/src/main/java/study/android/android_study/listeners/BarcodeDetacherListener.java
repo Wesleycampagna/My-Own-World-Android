2a5b9c8d44ac46fa6b64b95912101f7f761eedf8
Wesley Campagna <wesley.campagna@hotmail.com>
2020-11-09 04:02:18 -0400

    Add: cameraX artifacts | ML Kit Firebase to recognize Barcode and QRCode | Modularization to MVVM Future Reales

---
+++ app/src/main/java/study/android/android_study/listeners/BarcodeDetacherListener.java
@@ -0,0 +1,5 @@
+package study.android.android_study.listeners;
+
+public interface BarcodeDetacherListener {
+    void onClose();
+}
---
+++ app/src/main/java/study/android/android_study/modelview_view_model/CameraModelView.java
@@ -0,0 +1,4 @@
+package study.android.android_study.modelview_view_model;
+
+public class CameraModelView {
+}
